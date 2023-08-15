9bc73c802e59579f253cce9bd0753144988a9253
Sardor <106055019+sardorziyodullayev@users.noreply.github.com>
2023-08-15 11:42:19 +0500

    The booking page is ready

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Container, Row, Col, Form, FormGroup, Button } from "reactstrap";
+import { Link } from "react-router-dom";
+import "../style/login.css";
+
+import registerImg from "../assets/images/register.png";
+import userIcon from "../assets/images/user.png";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+	const [credentials, setCredentials] = useState({
+    userName: undefined,
+		email: undefined,
+		password: undefined,
+	});
 
-export default Register
+	const handleChange = e => {
+		setCredentials(prev => ({ ...prev, [e.target.id]: e.target.value }));
+	};
+
+	const handleClick = e => {
+		e.preventDefault();
+	};
+
+	return (
+		<section>
+			<Container>
+				<Row>
+					<Col lg="8" className="m-auto">
+						<div className="login__container d-flex justify-content-between">
+							<div className="login__img">
+								<img src={registerImg} alt="" />
+							</div>
+
+							<div className="login__form">
+								<div className="user">
+									<img src={userIcon} alt="" />
+								</div>
+								<h2>Register</h2>
+
+								<Form onClick={handleClick}>
+                <FormGroup>
+										<input
+											type="text"
+											placeholder="Username"
+											id="username"
+											onChange={handleChange}
+											required
+										/>
+									</FormGroup>
+									<FormGroup>
+										<input
+											type="email"
+											placeholder="Email"
+											id="email"
+											onChange={handleChange}
+											required
+										/>
+									</FormGroup>
+									<FormGroup>
+										<input
+											type="password"
+											placeholder="Password"
+											id="password"
+											onChange={handleChange}
+											required
+										/>
+									</FormGroup>
+									<Button className="btn secondary__btn auth__btn" type="submit">
+										Create Account
+									</Button>
+								</Form>
+								<p>
+									Already have an account? <Link to="/login">Login</Link>
+								</p>
+							</div>
+						</div>
+					</Col>
+				</Row>
+			</Container>
+		</section>
+	);
+};
+
+export default Register;
--- src/pages/TourDetails.jsx
+++ src/pages/TourDetails.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { useParams } from "react-router-dom";
 import { Container, Row, Col, Form, ListGroup } from "reactstrap";
 import "../style/tour-details.css";
-import { useParams } from "react-router-dom";
 import tourData from "../assets/data/tours";
 import canculateAvgRating from "../utils/avgRating";
+import avatar from "../assets/images/avatar.jpg";
+import Booking from "../components/Booking/Booking";
+import Newsletter from "../shared/Newsletter"
 
 const TourDetails = () => {
 	const { id } = useParams();
+	const reviewMsgRef = useRef("");
+	const [tourRating, setTourRating] = useState(null);
 
 	const tour = tourData.find(tour => tour.id === id);
 	const {
@@ -23,12 +28,24 @@ const TourDetails = () => {
 
 	const { avgRating, totalRating } = canculateAvgRating(reviews);
 
+	// format date
+	const options = { day: "numeric", month: "long", year: "numeric" };
+
+	// submit request to the server
+	const submitHundler = e => {
+		e.preventDefault();
+		const reviewText = reviewMsgRef.current.value;
+
+		// later will call our api
+		// alert(`${reviewText}, ${tourRating}`);
+	};
+
 	return (
 		<>
 			<section>
 				<Container>
 					<Row>
-						<Col>
+						<Col lg="8">
 							<div className="tour__content">
 								<img src={photo} alt="" />
 
@@ -41,7 +58,7 @@ const TourDetails = () => {
 												class="ri-star-fill"
 												style={{ color: "var(--secondary-color)" }}
 											></i>
-											{canculateAvgRating === 0 ? null : avgRating}
+											{avgRating === 0 ? null : avgRating}
 											{totalRating === 0 ? "Not rated" : <span>({reviews?.length})</span>}
 										</span>
 
@@ -55,9 +72,16 @@ const TourDetails = () => {
 										<span>
 											<i class="ri-map-pin-2-fill"></i>
 											{city}
+										</span>
+										<span>
 											<i class="ri-money-dollar-circle-line"></i> ${price} / per person
+										</span>
+										<span>
+											<i class="ri-map-pin-time-line"></i> {distance} km/h
+										</span>
+										<span>
 											<i class="ri-group-line"></i>
-											{maxGroupSize}
+											{maxGroupSize} people
 										</span>
 									</div>
 									<h5>Description</h5>
@@ -68,40 +92,72 @@ const TourDetails = () => {
 								<div className="tour__reviews mt-4">
 									<h4>Reviews ({reviews?.length} reviews)</h4>
 
-									<Form>
+									<Form onClick={submitHundler}>
 										<div className="d-flex align-items-center gap-3 mb-4 rating__group">
-											<span>
+											<span onClick={() => setTourRating(1)}>
 												1 <i class="ri-star-s-fill"></i>
 											</span>
-											<span>
+											<span onClick={() => setTourRating(2)}>
 												2 <i class="ri-star-s-fill"></i>
 											</span>
-											<span>
+											<span onClick={() => setTourRating(3)}>
 												3 <i class="ri-star-s-fill"></i>
 											</span>
-											<span>
+											<span onClick={() => setTourRating(4)}>
 												4 <i class="ri-star-s-fill"></i>
 											</span>
-											<span>
+											<span onClick={() => setTourRating(5)}>
 												5 <i class="ri-star-s-fill"></i>
 											</span>
 										</div>
 
 										<div className="review__input">
-											<input type="text" placeholder="share your thoughts" />
-											<button
-												className="btn primary__btn text-white"
-												type="submit14"
-											></button>
+											<input
+												type="text"
+												ref={reviewMsgRef}
+												placeholder="share your thoughts"
+												required/>
+											<button className="btn primary__btn text-white" type="submit">
+												Submit
+											</button>
 										</div>
 									</Form>
+
+									<ListGroup className="user__review">
+										{reviews.map(review => (
+											<div className="review__item">
+												<img src={avatar} alt="" />
+
+												<div className="w-100">
+													<div className="d-flex align-items-center justify-content-between">
+														<div>
+															<h5>John</h5>
+															<p>
+																{new Date("08-04-2023").toLocaleDateString("en-US", options)}
+															</p>
+														</div>
+														<span className="d-flex align-items-center">
+															5<i class="ri-star-s-fill"></i>
+														</span>
+													</div>
+
+													<h6>Amazing tour👌</h6>
+												</div>
+											</div>
+										))}
+									</ListGroup>
 								</div>
 								{/* ===========Tour reviews section end =========== */}
 							</div>
 						</Col>
+
+						<Col lg='4'>
+							<Booking tour={tour} avgRating={avgRating}/>
+						</Col>
 					</Row>
 				</Container>
 			</section>
+			<Newsletter />
 		</>
 	);
 };
